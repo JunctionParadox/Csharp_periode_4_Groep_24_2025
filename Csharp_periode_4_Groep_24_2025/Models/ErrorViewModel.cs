492b9c64a8a3a880bbf892821b4b25946827118c
JunctionParadox <Novasigma89@gmail.com>
2025-05-13 23:22:51 +0200

    Add project files.

---
+++ Csharp_periode_4_Groep_24_2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Csharp_periode_4_Groep_24_2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
