e1a9b1e3565cb9203cbfc9e7c21bff45ddb4c572
JunctionParadox <Novasigma89@gmail.com>
2025-05-14 00:45:12 +0200

    Minor start mssql database setup

---
+++ Csharp_periode_4_Groep_24_2025/Data/DbContext24.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Csharp_periode_4_Groep_24_2025.Data
+{
+    public class DbContext24 : DbContext
+    {
+        public DbContext24(DbContextOptions<DbContext> options) : base(options) { }
+    }
+}
